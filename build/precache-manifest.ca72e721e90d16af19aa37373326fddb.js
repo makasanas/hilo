bdc089ec890247417f3a1016254dd1d288d785a8
Sanjay Makasana <sanjaymakasana@Sanjays-MacBook-Air-2.local>
2019-04-12 10:24:08 +0530

    Initial dist subtree commit

---
+++ build/precache-manifest.ca72e721e90d16af19aa37373326fddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b531e70b975beab5d711",
+    "url": "/static/js/main.a8caabb5.chunk.js"
+  },
+  {
+    "revision": "e748b1195d4cdac070e0",
+    "url": "/static/js/2.366795e7.chunk.js"
+  },
+  {
+    "revision": "b531e70b975beab5d711",
+    "url": "/static/css/main.ed290d91.chunk.css"
+  },
+  {
+    "revision": "f87e63e1312e84aaca3594a30251574d",
+    "url": "/index.html"
+  }
+];
